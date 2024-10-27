e5b1cbb6e68198535d37b9410602de3a39cd7f08
devxnr <dleinweb@ualberta.ca>
2024-10-26 20:43:40 -0600

    nofifications adapter created

---
+++ SyzygyEvents/app/src/main/java/com/syzygy/events/ui/entrant/EntrantNotificationsAdapter.java
@@ -0,0 +1,57 @@
+package com.syzygy.events.ui.entrant;
+
+import com.syzygy.events.R;
+import com.syzygy.events.database.Notification;
+import com.syzygy.events.database.User;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class EntrantNotificationsAdapter extends ArrayAdapter<Notification> {
+
+    public EntrantNotificationsAdapter(Context context, ArrayList<Notification> dataList) {
+        super(context, 0, dataList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
+        Notification notification = getItem(position);
+
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(
+                    R.layout.item_entrant_notifications, parent, false);
+        }
+
+        TextView subject = view.findViewById(R.id.subject);
+        subject.setText(notification.getSubject());
+
+        TextView body = view.findViewById(R.id.body);
+        body.setText(notification.getBody());
+
+        TextView sender = view.findViewById(R.id.sender);
+        sender.setText(notification.getSender().getName());
+
+        TextView sent_date = view.findViewById(R.id.sent_date);
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        sent_date.setText(df.format(notification.getSentTime()));
+
+        //notification.getIsRead();
+
+
+        return view;
+    }
+
+
+}
